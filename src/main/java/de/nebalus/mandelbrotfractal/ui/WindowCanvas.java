49163be2738055713e8d457f9466c14128edca4b
Nebalus <56486387+Nebalus@users.noreply.github.com>
2024-02-20 18:41:42 +0100

    Double Buffer Fix

--- src/main/java/de/nebalus/mandelbrotfractal/ui/WindowCanvas.java
+++ src/main/java/de/nebalus/mandelbrotfractal/ui/WindowCanvas.java
@@ -1,13 +1,14 @@
 package de.nebalus.mandelbrotfractal.ui;
 
-import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Toolkit;
+import javax.swing.JPanel;
+
 import de.nebalus.mandelbrotfractal.renderer.MandelbrotRenderer;
 
-public class WindowCanvas extends Canvas
+public class WindowCanvas extends JPanel
 {
 
 	private static final long serialVersionUID = 8861427947706270401L;
@@ -21,38 +22,33 @@ public class WindowCanvas extends Canvas
 		this.renderer = new MandelbrotRenderer(windowWidth, windowHeigth);
 
 		this.showDebug = false;
+
+		super.setDoubleBuffered(true);
 	}
-	
+
 	@Override
-	public void paint(Graphics g)
+	public void paintComponent(Graphics g)
 	{
-		super.paint(g);
-
-		MandelbrotRenderer mbRenderer = (MandelbrotRenderer) renderer;
-
+		super.paintComponent(g);
+		
 		Long startProccesTimestamp = System.nanoTime();
-		g.drawImage(mbRenderer.render(), 0, 0, null);
+		g.drawImage(renderer.render(), 0, 0, null);
 		Long proccesTimeNanos = System.nanoTime() - startProccesTimestamp;
 
 		if (showDebug) {
 			g.setFont(new Font("Ink Free", Font.BOLD, 20));
 			g.setColor(Color.WHITE);
 
-			g.drawString("iterations: " + mbRenderer.getMaxIterations(), 10, g.getFont().getSize() + 10);
-			g.drawString("zoom: " + mbRenderer.getZoom(), 10, g.getFont().getSize() * 2 + 20);
-			g.drawString("xOffset: " + mbRenderer.getXOffset(), 10, g.getFont().getSize() * 3 + 30);
-			g.drawString("yOffset: " + mbRenderer.getYOffset(), 10, g.getFont().getSize() * 4 + 40);
+			g.drawString("iterations: " + renderer.getMaxIterations(), 10, g.getFont().getSize() + 10);
+			g.drawString("zoom: " + renderer.getZoom(), 10, g.getFont().getSize() * 2 + 20);
+			g.drawString("xOffset: " + renderer.getXOffset(), 10, g.getFont().getSize() * 3 + 30);
+			g.drawString("yOffset: " + renderer.getYOffset(), 10, g.getFont().getSize() * 4 + 40);
 			g.drawString("frameRendertimeNanos: " + proccesTimeNanos, 10, g.getFont().getSize() * 5 + 50);
 			g.drawString("frameRendertimeMillis: " + (proccesTimeNanos / 1000000), 10, g.getFont().getSize() * 6 + 60);
 		}
 		
 		Toolkit.getDefaultToolkit().sync();
 		g.dispose();
-	}
-
-	public void repaint()
-	{
-		super.repaint();
 	}
 
 	public MandelbrotRenderer getMandelbrotRenderer()
